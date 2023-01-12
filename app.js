e38e4a79ef5d2e2875d633780b30ff1ad910a478
DannyJLC <danny12349791@gmail.com>
2023-01-12 00:24:17 -0800

    i did stuff

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
 })
+
+
+function toggleSection() {
+    var x = document.getElementById("learn_more");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
